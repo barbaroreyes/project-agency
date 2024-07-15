51999546bd17a3f25e652bcb52dde0d55ba093bf
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2024-07-15 13:50:50 -0500

    connect to mongo

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
 import express from 'express';
 import morgan from 'morgan'
-
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config()
 
 
 const app = express();
-const PORT = 4500
+const PORT = process.env.PORT ||4500
 
  app.use(express.json());
  app.use(morgan("dev"));
- 
+
+
 app.get('/', (req, res) => {
     res.json("Hello");
 })
 
-
-app.listen(PORT ,()=>{
-    console.log(`Listening on port ${PORT}`);
+mongoose.connect(process.env.MONDODBURI)
+.then(()=>{
+    console.log("Connected to MongoDB")
+    app.listen(PORT ,()=>{
+        console.log(`Listening on port ${PORT}`);
+    })
 })
+
