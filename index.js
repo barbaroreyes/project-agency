a2e062cff030651735e2ba95cce9e66500862ad4
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2024-07-16 11:46:38 -0500

    update-cors

--- index.js
+++ index.js
@@ -9,9 +9,10 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 4500;
 
+app.use(cors())
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(cors())
+
 
 app.use("/api",routerAgency );
 mongoose.connect(process.env.MONDODBURI)
